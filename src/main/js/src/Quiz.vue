74e9bf30d39e09038b5b088079440bfbe72e382d
Mark Haynes <mark.haynes@winnowsolutions.com>
2018-10-08 21:48:51 +0100

    addition of a question component

---
+++ src/main/js/src/Quiz.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <question
+            question="Success is not final, failure is not fatal: it is the courage to continue that counts."
+            :answers="['Winston S. Churchill','Virginia Woolf','Albert Einstein','Anais Nin']"
+        />
+    </div>
+</template>
+
+<script>
+    import Question from './Question.vue';
+
+    export default {
+        components: { Question }
+    }
+</script>
